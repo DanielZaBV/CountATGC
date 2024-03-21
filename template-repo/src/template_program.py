7d6421cdbec813b274dc1759185c4983855e4c99
DanielZBV <danielzb@lcg.unam.mx>
2024-03-21 13:20:36 -0600

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- template-repo/src/template_program.py
+++ template-repo/src/template_program.py
@@ -1,5 +1,5 @@
 '''
-NAME: 
+NAME: Daniel Bautista Vargas Daniel Zaid
        
 
 VERSION:
@@ -31,7 +31,25 @@ SEE ALSO
         
 '''
 
+def count_nucleotides(dna_sequence):
+    nucleotide_count = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
+    
+    for nucleotide in dna_sequence:
+        if nucleotide in nucleotide_count:
+            nucleotide_count[nucleotide] += 1
+    
+    return nucleotide_count
 
+# Solicitar al usuario que ingrese la secuencia de ADN
+dna_sequence = input("Ingresa la secuencia de ADN: ")
+
+# Contar los nucleótidos en la secuencia de ADN proporcionada
+result = count_nucleotides(dna_sequence)
+
+# Mostrar los resultados
+print("Recuento de nucleótidos en la secuencia de ADN:")
+for nucleotide, count in result.items():
+    print(f"{nucleotide}: {count}")
 # ===========================================================================
 # =                            imports
 # ===========================================================================
